055d2e7952b16cc7f615639d2b87460020e851e6
arishimam <62866346+arishimam@users.noreply.github.com>
2023-04-11 11:06:25 -0700

    update db

--- app.py
+++ app.py
@@ -25,7 +25,7 @@ from flask_jwt_extended import (
 import re
 
 app = Flask(__name__)
-app.config["MYSQL_HOST"] = "db"
+app.config["MYSQL_HOST"] = "localhost"
 app.config["MYSQL_USER"] = "root"
 app.config["MYSQL_PASSWORD"] = "root"
 app.config["MYSQL_DB"] = "web"
